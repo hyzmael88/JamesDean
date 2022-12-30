27baf0a4db7b1476933479e484103595bdaaca0e
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2022-12-29 20:47:10 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Navbar from './components/Navbar'
-import Home from './Pages/Home'
+import Home from './pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
